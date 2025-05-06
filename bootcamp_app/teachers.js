5d368c2bdac10a5c488f566f945c930929bd1bc4
JaxonC-06 <jaxoncostain@outlook.com>
2025-05-06 12:50:32 -0600

    Worked with Node Postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+const { Pool } = require("pg");
+
+const pool = new Pool ({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool
+  .query(
+    `
+    SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.teacher_id) AS total_assistances
+    FROM assistance_requests
+    JOIN students ON students.id = student_id
+    JOIN teachers ON teachers.id = teacher_id
+    JOIN cohorts ON cohorts.id = students.cohort_id
+    WHERE cohorts.name = '${process.argv[2]}'
+    GROUP BY teachers.name, cohorts.name
+    ORDER BY teachers.name;
+    `
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.cohort}: ${user.teacher}`
+      )
+    })
+  })
